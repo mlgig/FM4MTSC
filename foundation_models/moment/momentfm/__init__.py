15221fff7068a2f2064542677bf6c67d8523f180
psungu <pinar.sunguisiacik@ucdconnect.ie>
2025-07-08 14:48:54 +0100

    Update moment model experiments

--- foundation_models/moment/momentfm/__init__.py
+++
@@ -1,1 +0,0 @@
-from .models.moment import MOMENT, MOMENTPipeline
--- foundation_models/moment/momentfm/common.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class TASKS:
-    RECONSTRUCTION: str = "reconstruction"
-    FORECASTING: str = "forecasting"
-    CLASSIFICATION: str = "classification"
-    EMBED: str = "embedding"
